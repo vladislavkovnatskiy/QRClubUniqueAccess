083de96f43dc828b1eadde6e4ca901a5377e45af
vladislavkovnatskiy <i@vvkov.ru>
2026-05-31 22:36:51 +0300

    first commit

---
+++ src/main/java/org/example/qrclubuniqueaccess/QrclubuniqueaccessApplication.java
@@ -0,0 +1,13 @@
+package org.example.qrclubuniqueaccess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrclubuniqueaccessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QrclubuniqueaccessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/qrclubuniqueaccess/QrclubuniqueaccessApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.qrclubuniqueaccess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrclubuniqueaccessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
